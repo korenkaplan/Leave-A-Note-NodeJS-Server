276a8439484efb0269721291a8b96f700b8dea88
koren_kaplan <korenkaplan96@gmail.com>
2023-06-26 14:43:43 +0300

    fix build in git ignore

--- build/resources/unMatchedReports/unMatchedReports.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const UnMatchedReportsSchema = new mongoose_1.Schema({
-    accident: { type: Object, required: true },
-    damagedCarNumber: { type: String, required: true },
-}, { collection: 'unMatchedReports' });
-exports.default = (0, mongoose_1.model)('UnMatchedReports', UnMatchedReportsSchema);
