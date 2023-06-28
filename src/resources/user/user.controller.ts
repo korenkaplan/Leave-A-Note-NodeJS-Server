766a7f273f457b4ef776ffce1981d307c47b98f1
koren_kaplan <korenkaplan96@gmail.com>
2023-06-28 20:18:49 +0300

    register function edit

--- src/resources/user/user.controller.ts
+++ src/resources/user/user.controller.ts
@@ -18,10 +18,10 @@ class UserController implements IController {
     private initializeRoutes(): void {
         this.router.post(`${this.path}/register`, validationMiddleware(validate.register), this.register);
         this.router.post(`${this.path}/login`, validationMiddleware(validate.login), this.login);
-        this.router.post(`${this.path}/passwordUpdate`,authenticated,validationMiddleware(validate.passwordUpdate), this.updateUserPassword);
-        this.router.post(`${this.path}/informationUpdate`,authenticated,validationMiddleware(validate.infoUpdate), this.updateUserInformation);
-        this.router.post(`${this.path}/deleteMessage`,authenticated, validationMiddleware(validate.deleteMessage), this.deleteMessageById);
-        this.router.post(`${this.path}/deleteMessageInbox`,authenticated, validationMiddleware(validate.deleteMessage), this.deleteMessageInbox);
+        this.router.post(`${this.path}/passwordUpdate`, authenticated, validationMiddleware(validate.passwordUpdate), this.updateUserPassword);
+        this.router.post(`${this.path}/informationUpdate`, authenticated, validationMiddleware(validate.infoUpdate), this.updateUserInformation);
+        this.router.post(`${this.path}/deleteMessage`, authenticated, validationMiddleware(validate.deleteMessage), this.deleteMessageById);
+        this.router.post(`${this.path}/deleteMessageInbox`, authenticated, validationMiddleware(validate.deleteMessage), this.deleteMessageInbox);
         this.router.post(`${this.path}/getUser`, authenticated, this.getUserQuery);
     }
     // ** Checked
@@ -29,12 +29,13 @@ class UserController implements IController {
         try {
             const { name, email, password, carNumber, phoneNumber } = req.body;
             const token = await this.UserService.register(name, email, carNumber, phoneNumber, password, 'user');
-            const [ message, success, status, data ] = ['Register Successfully',true,201,token]
-            const resBody: IHttpResponse<string> = {message,success,data}
+            const [message, success, status, data] = ['Register Successfully', true, 201, token]
+            const resBody: IHttpResponse<string> = { message, success, data }
             res.status(status).json(resBody)
 
         } catch (error: any) {
-            const resBody: IHttpResponse<string> = {message:'Server Error', success:false,error: error.message}
+            const fieldInUse = this.UserService.translateError(error.message)
+            const resBody: IHttpResponse<string> = { message: 'Failed to update user info', success: false, error:`This ${fieldInUse} is already in use.` }
             res.status(500).json(resBody)
         }
     };
@@ -42,82 +43,82 @@ class UserController implements IController {
     private login = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
         try {
             const { email, password } = req.body;
-            const [isSuccessful,tokenOrError] = await this.UserService.login(email, password);
-            const [ message, success, status, data, error ] = isSuccessful? ['Successfully Identified', true, 200, {token: tokenOrError}] : ['Invalid credentials', false,404,,tokenOrError]
-            const resBody: IHttpResponse<object> = {message, success, data, error}
+            const [isSuccessful, tokenOrError] = await this.UserService.login(email, password);
+            const [message, success, status, data, error] = isSuccessful ? ['Successfully Identified', true, 200, { token: tokenOrError }] : ['Invalid credentials', false, 404, , tokenOrError]
+            const resBody: IHttpResponse<object> = { message, success, data, error }
             return res.status(status).json(resBody)
         } catch (error: any) {
-            res.status(500).json({message:'Server Error', success:false,error: error.message} as IHttpResponse<void>)
+            res.status(500).json({ message: 'Server Error', success: false, error: error.message } as IHttpResponse<void>)
         }
     };
-     // ** Checked
+    // ** Checked
     private getUserQuery = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
         try {
             const { query, projection } = req.body;
             const user: IUser | null = await this.UserService.GetUserQuery(query, projection);
-            const [message,success,status,data] = user !== null? ['success',true,200,user]: ['User Not Found',false,404]
-            const resBody: IHttpResponse<IUser> = {message,success,data}
+            const [message, success, status, data] = user !== null ? ['success', true, 200, user] : ['User Not Found', false, 404]
+            const resBody: IHttpResponse<IUser> = { message, success, data }
             res.status(status).json(resBody)
         } catch (error: any) {
-            const resBody: IHttpResponse<string> = {message:'Error fetching information',error: error.message,  success: false};
+            const resBody: IHttpResponse<string> = { message: 'Error fetching information', error: error.message, success: false };
             res.status(500).json(resBody)
         }
     }
-     // ** Checked
+    // ** Checked
     private deleteMessageById = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
         try {
             const { userId, messageId } = req.body;
-            const [isSuccessful,resultMessage] = await this.UserService.deleteMessage(userId, messageId);
-            const [message,status, error] = isSuccessful? [resultMessage,200]: ['Failed to delete message',404,resultMessage];
-            const resBody: IHttpResponse<void> = {success: isSuccessful,message, error}
-             res.status(status).json(resBody);
+            const [isSuccessful, resultMessage] = await this.UserService.deleteMessage(userId, messageId);
+            const [message, status, error] = isSuccessful ? [resultMessage, 200] : ['Failed to delete message', 404, resultMessage];
+            const resBody: IHttpResponse<void> = { success: isSuccessful, message, error }
+            res.status(status).json(resBody);
         } catch (error: any) {
-            const resBody: IHttpResponse<void> = {message:'Failed to delete message',error: error.message,success:false}
+            const resBody: IHttpResponse<void> = { message: 'Failed to delete message', error: error.message, success: false }
             res.status(500).json(resBody);
         }
     };
-         // ** Checked
-         private deleteMessageInbox = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
-            try {
-                const { userId, messageId } = req.body;
-                const [isSuccessful,resultMessage] = await this.UserService.deleteMessageInbox(userId, messageId);
-                const [message,status, error] = isSuccessful? [resultMessage,200]: ['Failed to delete message',404,resultMessage];
-                const resBody: IHttpResponse<void> = {success: isSuccessful,message, error}
-                 res.status(status).json(resBody);
-            } catch (error: any) {
-                const resBody: IHttpResponse<void> = {message:'Failed to delete message',error: error.message,success:false}
-                res.status(500).json(resBody);
-            }
-        };
-     // ** Checked
+    // ** Checked
+    private deleteMessageInbox = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+        try {
+            const { userId, messageId } = req.body;
+            const [isSuccessful, resultMessage] = await this.UserService.deleteMessageInbox(userId, messageId);
+            const [message, status, error] = isSuccessful ? [resultMessage, 200] : ['Failed to delete message', 404, resultMessage];
+            const resBody: IHttpResponse<void> = { success: isSuccessful, message, error }
+            res.status(status).json(resBody);
+        } catch (error: any) {
+            const resBody: IHttpResponse<void> = { message: 'Failed to delete message', error: error.message, success: false }
+            res.status(500).json(resBody);
+        }
+    };
+    // ** Checked
     private updateUserPassword = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
         try {
-          const {userId, oldPassword, newPassword} = req.body; 
-          const [success,resultMessage,status]= await this.UserService.updateUserPassword(userId, oldPassword, newPassword);
-         const [message, error] = success ? [resultMessage]:['Failed To Update Password',resultMessage] 
-          const resBody: IHttpResponse<void> = {success,message,error}
-          console.log(error);
-          
-          return res.status(status).json(resBody);
+            const { userId, oldPassword, newPassword } = req.body;
+            const [success, resultMessage, status] = await this.UserService.updateUserPassword(userId, oldPassword, newPassword);
+            const [message, error] = success ? [resultMessage] : ['Failed To Update Password', resultMessage]
+            const resBody: IHttpResponse<void> = { success, message, error }
+            console.log(error);
+
+            return res.status(status).json(resBody);
         } catch (error: any) {
-          const resBody: IHttpResponse<void> = {message:'Error updating user password',error:error.message,success:false}
+            const resBody: IHttpResponse<void> = { message: 'Error updating user password', error: error.message, success: false }
             res.status(500).json(resBody)
         }
     };
-     // ** Checked
+    // ** Checked
     private updateUserInformation = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
         try {
-            const {userId, update} = req.body;
-            const [isSuccessful,resultMessage] = await this.UserService.updateUserInfo(userId,update);
-            const [success,message,status,error] = isSuccessful? [true,resultMessage,200]:[false,'Failed to update user\'s info',400,resultMessage];
-            const resBody: IHttpResponse<void> = {message,success,error}
+            const { userId, update } = req.body;
+            const [isSuccessful, resultMessage] = await this.UserService.updateUserInfo(userId, update);
+            const [success, message, status, error] = isSuccessful ? [true, resultMessage, 200] : [false, 'Failed to update user\'s info', 400, resultMessage];
+            const resBody: IHttpResponse<void> = { message, success, error }
             return res.status(status).json(resBody);
         } catch (e: any) {
-           const fieldInUse = this.UserService.translateError(e.message);
-            const resBody: IHttpResponse<void> = {success:false,message:'Failed to update user info',error:`This ${fieldInUse} is already in use`}
+            const fieldInUse = this.UserService.translateError(e.message);
+            const resBody: IHttpResponse<void> = { success: false, message: 'Failed to update user info', error: `This ${fieldInUse} is already in use` }
             res.status(500).json(resBody)
         }
     };
-  
+
 };
 export default UserController;
